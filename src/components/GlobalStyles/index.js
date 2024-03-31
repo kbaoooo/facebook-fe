c6e54e46a8ecb15b925a82f741c8aad6bcc3f303
kbao <kbao122003@gmail.com>
2024-04-01 01:31:17 +0700

    config and based layout, page

---
+++ src/components/GlobalStyles/index.js
@@ -0,0 +1,8 @@
+import './GlobalStyles.scss';
+
+function GlobalStyles({ children }) {
+    console.log(children);
+    return children;
+}
+
+export default GlobalStyles;
---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import Home from '~/pages/Home';
+import Authentication from '~/pages/Authentication';
+import { AuthenticationLayout, DefaultLayout } from '~/components/Layout';
+
+//public routes
+const publicRoutes = [
+    { path: '/', page: Home, layout: DefaultLayout },
+    { path: '/auth', page: Authentication, layout: AuthenticationLayout },
+];
+
+// private routes
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
